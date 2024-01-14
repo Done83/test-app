700085928c6e5e0cd0a32071945f9aee52d27d04
Done83 <nedomehan@gmail.com>
2024-01-14 13:37:50 +0100

    Remove files and add conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ToDoList from './components/ToDoList'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let randomBool = Math.random() > 0.5;
+    let headerOne = <h1>To do App</h1>;
+    let alternative = <div>To do Form</div>;
+    return (
+        <div>
+            {randomBool && headerOne}
+            {!randomBool && alternative}
+            <ToDoList />
+        </div>
+    )
 }
 
-export default App;
+export default App
